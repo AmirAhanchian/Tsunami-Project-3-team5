6b5df33f8154fbdab197715175fe61f6278c0e49
AmirAhanchian <Amir.ahanchi@gmail.com>
2023-02-26 22:21:33 -0500

    final chart js files updated

--- Chart JS visualization/logic.js
+++ Chart JS visualization/logic.js
@@ -1,5 +1,9 @@
 
 
+
+
+
+//$.getJSON("https://amirahanchian.github.io/Resources/Cleaned_Data_intensity.json", function(data) {
 $.getJSON("http://127.0.0.1:5000/api/v1.0/Tsunami", function(data) {
     //console.log(data);
 
@@ -13,27 +17,35 @@ $.getJSON("http://127.0.0.1:5000/api/v1.0/Tsunami", function(data) {
    
    cause = _.countBy(tsunami_info, function(tsunami_info) { return tsunami_info['Tsunami Cause Code']; });
       
-
+// replace and rename keys from numbers into actual tsunami causes (named)
    delete Object.assign(cause, { "Earthquake": cause['1'] })['1'];
    delete Object.assign(cause, { "Earthquake and Landslide": cause['3'] })['3'];
    delete Object.assign(cause, { "Volcano": cause['6'] })['6'];
    delete Object.assign(cause, { "Meteorological": cause['9'] })['9'];
 
-   console.log(cause); // { newKey: 'value' }
+   //check that it works
+   //console.log(cause);
 
 
-   
-   cause2 = {...cause, "Questionable Earthquake": 0, "Volcano and Earthquake":0, "Volcano, Earthquake, and Landslide":0, "Volcano and Landslide":0, "Landslide":0, "unknown": 0};
-   console.log(cause)
+   // create new keys to add all causes for tsunamis that were zero in our dataset 
+   // but are potential causes, for better info in the graphs
 
+   cause2 = {...cause, "Questionable Earthquake": 0, "Volcano and Earthquake":0, "Volcano, Earthquake, and Landslide":0, "Volcano and Landslide":0, "Landslide":0};
+   //console.log(cause2)
+
+
+   // create an opject of counts for each country to detrmine frequency of tsunami occurance by country
    country = _.countBy(tsunami_info, function(tsunami_info) { return tsunami_info['Country']; });
+   
+   // create an object for frequency of tsunamis by year
    year = _.countBy(tsunami_info, function(tsunami_info) { return tsunami_info['Year']; });
 
 
 
 
-
-   const ctx = document.getElementById('myChart');
+   // create charts by using chart.js for each graph.chart required
+   
+   const ctx = document.getElementById('myChart').getContext("2d");
    var chart = new Chart(ctx, {
       type: 'bar',
       data: {
@@ -46,6 +58,48 @@ $.getJSON("http://127.0.0.1:5000/api/v1.0/Tsunami", function(data) {
       },
       options: {
          responsive: 'true',
+         scales: {
+               
+            y: {
+               ticks: {
+                  font: {
+                      size: 14,
+                  }
+               },
+              title: {
+                display: true,
+                text: 'Frequency',
+                font: {
+                  size: 16,
+               }
+              }
+            },
+            x: {
+               ticks: {
+                  font: {
+                      size: 14,
+                  }
+               },
+               title: {
+                 display: true,
+                 text: 'Cause Name',
+                 font: {
+                  size: 16,
+                  }
+               }
+             }
+         },     
+         plugins:{
+            title: {
+               display: true,
+               text: 'Tsunami Causes',
+               font: {
+                  size: 22,
+               }
+            },
+            legend: {
+                display: false,}     
+         }
       }
    
    });
@@ -53,33 +107,119 @@ $.getJSON("http://127.0.0.1:5000/api/v1.0/Tsunami", function(data) {
    const ctx2 = document.getElementById('myChart2');
    var chart2 = new Chart(ctx2, {
       type: 'bar',
+      
       data: {
          
          datasets: [{
-            backgroundColor: 'rgb(129, 198, 2228)',
+            backgroundColor: 'rgb(1000, 0, 2)',
             borderColor: 'rgb(0, 150, 215)',
             data: country
          }]
       },
       options: {
          responsive: 'true',
+         scales: {
+               
+            y: {
+               ticks: {
+                  font: {
+                      size: 14,
+                  }
+               },
+              title: {
+                display: true,
+                text: 'Frequency',
+                font: {
+                  size: 16,
+                }
+              }
+            },
+            x: {
+               ticks: {
+                  font: {
+                      size: 14,
+                  }
+               },
+               title: {
+                 display: true,
+                 text: 'Country Name',
+                 font: {
+                     size: 16,
+                  }
+               }
+             }
+         },  
+         plugins:{
+            title: {
+               display: true,
+               text: 'Tsunami Frequency by Country',
+               font: {
+                  size: 22,
+               }
+            },
+            legend: {
+                display: false,}     
+         }
       }
-   
    });
 
    const ctx3 = document.getElementById('myChart3');
    var chart3 = new Chart(ctx3, {
-      type: 'bar',
+      type: 'line',
       data: {
          
          datasets: [{
-            backgroundColor: 'rgb(129, 198, 2228)',
+            backgroundColor: 'rgb(0, 198, 2228)',
             borderColor: 'rgb(0, 150, 215)',
             data: year
+            
          }]
       },
       options: {
          responsive: 'true',
+         scales: {
+               
+            y: {
+               ticks: {
+                  font: {
+                      size: 14,
+                  }
+               },
+              title: {
+                display: true,
+                text: 'Frequency',
+                font: {
+                  size: 16,
+               }
+              }
+            },
+            x: {
+               ticks: {
+                  font: {
+                      size: 14,
+                  }
+               },
+               title: {
+                 display: true,
+                 text: 'Year of Occurance',
+                 font: {
+                     size: 16,
+                  }
+               }
+             }
+            },  
+         plugins:{
+            title: {
+               display: true,
+               text: 'Tsunami Frequency by Year',
+               font: {
+                  size: 22,
+               }
+            },
+            legend: {
+                display: false},
+                 
+         }
       }
    
    });
